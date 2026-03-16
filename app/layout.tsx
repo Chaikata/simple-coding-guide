713f5d11d632ead2e745a1a5a253f1b68760b226
Chaikata <chaiyonatan@gmail.com>
2026-03-16 18:45:23 -0400

    "fix hydration error with analytics and adsense"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from "next/script";
 
 export const metadata = {
   title: "Dev Nest Guide",
@@ -14,36 +15,36 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-
+      <body className="bg-black text-white">
+        
         {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FBCSYKF0J8"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-FBCSYKF0J8');
-            `,
-          }}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FBCSYKF0J8"
+          strategy="afterInteractive"
         />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FBCSYKF0J8');
+          `}
+        </Script>
 
         {/* Google AdSense */}
-        <script
+        <Script
           async
+          strategy="afterInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4565007159817590"
           crossOrigin="anonymous"
-        ></script>
+        />
 
-      </head>
-
-      <body className="bg-black text-white">
         <div className="flex min-h-screen flex-col">
           <Navbar />
-          <div className="flex-1">{children}</div>
+          <main className="flex-1">{children}</main>
           <Footer />
         </div>
+
       </body>
     </html>
   );
